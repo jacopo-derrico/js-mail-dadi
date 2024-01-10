e6f7e94fcb5509daac2127709819912a924aa904
jacopo-derrico <derricoj.design@gmail.com>
2024-01-10 16:15:59 +0100

    corrected function w/ control variable

--- mail_access/js/main.js
+++ mail_access/js/main.js
@@ -3,23 +3,32 @@
 // controllo se la mail è nella lista
 // fornisco feedback
 
-const emailProfiles = ["first@email.com", "second@email.com", "third@email.com", "forth@email.com" ]
+const emailProfiles = ["first@email.com", "second@email.com", "third@email.com", "forth@email.com"]
 const inputMail = document.getElementById('mail')
 const checkBtn = document.getElementById('check-btn')
 const resultCheck = document.getElementById('result')
 
 for (let i = 0; i < emailProfiles.length; i++) {
-    console.log(emailProfiles[i])   
+    console.log(emailProfiles[i])
 }
 
 checkBtn.addEventListener('click', checkMail)
 
+let controlVariable = false
+
+
 function checkMail() {
     for (let i = 0; i < emailProfiles.length; i++) {
-        
+
         if (emailProfiles[i] === inputMail.value) {
-            resultCheck.innerHTML = "Mail corretta"
+            controlVariable = true
         }
-        
     }
+
+    if (controlVariable == true) {
+        resultCheck.innerHTML = "Benvenut*"
+    } else {
+        resultCheck.innerHTML = "Mi dispiace, festa privata"
+    }
+
 }
